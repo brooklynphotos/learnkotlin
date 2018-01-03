173eefe7ea06ad33e7763b2e911a24f630ecb318
Brooklyn Photos <guoneng.list@gmail.com>
2018-01-02 23:39:02 -0500

    added documentation

--- src/main/kotlin/photos/brooklyn/learnkotlin/service/StaticDataService.kt
+++ src/main/kotlin/photos/brooklyn/learnkotlin/service/StaticDataService.kt
@@ -1,5 +1,8 @@
 package photos.brooklyn.learnkotlin.service
 
+/**
+ * service to use a static system
+ */
 interface StaticDataService {
     /**
      * retrieves data based on the key for that data
